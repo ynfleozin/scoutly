812f0d87589069003e385ab4b1d3fa0f196e84ef
ynfleozin <leonardoalvarengam@gmail.com>
2026-05-23 23:55:19 -0300

    chore: initial spring boot project setup

---
+++ scoutly-backend/src/main/java/com/leonardoalvarenga/scoutly/ScoutlyApplication.java
@@ -0,0 +1,13 @@
+package com.leonardoalvarenga.scoutly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoutlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoutlyApplication.class, args);
+	}
+
+}
---
+++ scoutly-backend/src/test/java/com/leonardoalvarenga/scoutly/ScoutlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardoalvarenga.scoutly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoutlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
