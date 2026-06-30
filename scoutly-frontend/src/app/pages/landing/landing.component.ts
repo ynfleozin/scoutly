91d0e44e9748bc7073a413dbeda01126b7cfe5f8
ynfleozin <leonardoalvarengam@gmail.com>
2026-06-30 15:16:20 -0300

    feat: show different landing actions for logged users

--- scoutly-frontend/src/app/pages/landing/landing.component.ts
+++ scoutly-frontend/src/app/pages/landing/landing.component.ts
@@ -1,17 +1,24 @@
-import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit {
   private authService = inject(AuthService);
   private router = inject(Router);
+
+  isUserLoggedIn: boolean = false;
+
+  ngOnInit(): void {
+    this.isUserLoggedIn = this.authService.isLoggedIn();
+  }
 
   startDemo(): void {
     this.authService.guestLogin().subscribe({
